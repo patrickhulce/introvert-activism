1b31830059d69968a7317859f8afe4a2a469c3e7
Connor Clark <cjamcl@gmail.com>
2020-06-04 21:33:56 -0700

    recording works

--- packages/frontend/src/messages/record-message-screen.tsx
+++ packages/frontend/src/messages/record-message-screen.tsx
@@ -1,87 +1,118 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
 import Meyda from 'meyda'
 import * as React from 'react'
 
-// Hooks from https://stackoverflow.com/questions/57298567/correct-handling-of-react-hooks-for-microphone-audio
+type MediaRecorder = any // TODO
 
-const getMedia = async () => {
+const getMedia = async (constraints: MediaStreamConstraints) => {
   try {
-    return await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: false,
-    })
+    return await navigator.mediaDevices.getUserMedia(constraints)
   } catch (err) {
     console.log('Error:', err)
   }
 }
 
-const useMeydaAnalyser = audio => {
-  const [analyser, setAnalyser] = React.useState(null)
-  const [running, setRunning] = React.useState(false)
-  const [features, setFeatures] = React.useState(null)
-  const [stream, setStream] = React.useState(null)
+function useRecorder(recorder) {
+  const [recording, setRecording] = React.useState(false)
 
   React.useEffect(() => {
+    if (!recorder) return
+
+    if (recording && recorder.state === 'inactive') {
+      recorder.start(100)
+    } else if (!recording && recorder.state === 'recording') {
+      recorder.stop()
+    }
+  }, [recording, recorder])
+
+  return [recording, setRecording]
+}
+
+const useMeydaAnalyser = (stream, recording) => {
+  const [features, setFeatures] = React.useState(null)
+
+  let analyzer
+  React.useEffect(() => {
+    if (!stream || !recording) return
+
     const audioContext = new AudioContext()
 
-    let newAnalyser
-    getMedia().then(stream => {
-      setStream(stream)
-      if (audioContext.state === 'closed') {
-        return
-      }
-      const source = audioContext.createMediaStreamSource(stream)
-      newAnalyser = Meyda.createMeydaAnalyzer({
-        audioContext,
-        source,
-        bufferSize: 1024,
-        featureExtractors: ['amplitudeSpectrum', 'mfcc', 'rms'],
-        callback: features => {
-          setFeatures(features)
-        },
-      })
-      setAnalyser(newAnalyser)
+    const source = audioContext.createMediaStreamSource(stream)
+    console.log('...')
+    analyzer = Meyda.createMeydaAnalyzer({
+      audioContext,
+      source,
+      bufferSize: 1024,
+      featureExtractors: ['amplitudeSpectrum', 'mfcc', 'rms'],
+      callback: features => {
+        setFeatures(features)
+      },
     })
+    analyzer.start()
+
     return () => {
-      if (newAnalyser) {
-        newAnalyser.stop()
-      }
-      if (audioContext) {
-        audioContext.close()
-      }
+      if (analyzer) analyzer.stop()
+      if (audioContext) audioContext.close()
     }
-  }, [])
+  }, [stream, recording])
 
-  React.useEffect(() => {
-    if (analyser) {
-      if (running) {
-        analyser.start()
-        audio.current.srcObject = null
-      } else {
-        analyser.stop()
-        // TODO: audio isn't right ... lol
-        audio.current.srcObject = stream
-      }
-    }
-  }, [running, analyser, stream])
-
-  return [running, setRunning, features]
+  return features
 }
 
 export const RecordMessageScreen = () => {
   const levelRange = React.useRef(null)
   const audio = React.useRef(null)
 
-  // TODO: why are these typed as any?
-  const [running, setRunning, features] = useMeydaAnalyser(audio)
+  const mediaStream = React.useRef<MediaStream | null>(null)
+  const mediaRecorder = React.useRef<MediaRecorder | null>(null)
+  const mediaChunks = React.useRef<Blob[]>([])
+
+  const getMediaStream = React.useCallback(async () => {
+    mediaStream.current = await getMedia({
+      audio: true,
+      video: false,
+    })
+  }, [])
+
+  const getMediaRecorder = React.useCallback(async () => {
+    if (!mediaStream.current) await getMediaStream()
+
+    // @ts-ignore
+    const recorder = new MediaRecorder(mediaStream.current)
+    recorder.ondataavailable = e => {
+      mediaChunks.current.push(e.data)
+    }
+    recorder.onerror = console.error
+    mediaRecorder.current = recorder
+  }, [mediaStream.current])
+
+  const [recording, setRecording] = useRecorder(mediaRecorder.current)
 
   React.useEffect(() => {
+    if (!mediaRecorder.current) return
+
+    if (recording) {
+      audio.current.src = ''
+      mediaChunks.current = []
+    } else {
+      const blob = new Blob(mediaChunks.current, {type: 'audio/ogg; codecs=opus'})
+      audio.current.src = URL.createObjectURL(blob)
+    }
+  }, [recording])
+
+  const features = useMeydaAnalyser(mediaStream.current, recording)
+  React.useEffect(() => {
+    console.log(features)
     if (levelRange.current && features) {
       levelRange.current.value = features.rms
     }
   }, [features])
 
-  const handleClick = () => {
-    setRunning(running => !running)
+  const handleClick = async () => {
+    if (!mediaRecorder.current) await getMediaRecorder()
+    // @ts-ignore ?
+    setRecording(recording => !recording)
   }
 
   return (
@@ -90,7 +121,7 @@ export const RecordMessageScreen = () => {
       <textarea rows={20} cols={50}>
         Write your script here so you can read as you record.
       </textarea>
-      <button onClick={handleClick}>{!running ? 'Start' : 'Stop'} Recording</button>
+      <button onClick={handleClick}>{!recording ? 'Start' : 'Stop'} Recording</button>
       <audio ref={audio} controls></audio>
       <input
         ref={levelRange}
