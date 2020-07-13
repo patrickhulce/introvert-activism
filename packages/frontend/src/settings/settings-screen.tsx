ccbf7d78089e7cd3c4df86e892dcb71f6e198689
Patrick Hulce <patrick.hulce@gmail.com>
2020-07-13 16:47:11 -0500

    feat(frontend): add settings token tester

--- packages/frontend/src/settings/settings-screen.tsx
+++ packages/frontend/src/settings/settings-screen.tsx
@@ -3,10 +3,21 @@ import * as React from 'react'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
+import CheckIcon from '@material-ui/icons/Check'
+import ErrorIcon from '@material-ui/icons/Error'
+import HourglassIcon from '@material-ui/icons/HourglassEmpty'
 
 import {useUserSettings} from './use-user-settings'
 
+enum TestState {
+  NotStarted = 'ns',
+  Started = 's',
+  Passed = 'p',
+  Failed = 'f',
+}
+
 export const SettingsScreen = (): JSX.Element => {
+  const [testState, setTestState] = React.useState(TestState.NotStarted)
   const [settings, setSettings] = useUserSettings()
   const [formValues, setFormValues] = React.useState(settings)
 
@@ -40,7 +51,8 @@ export const SettingsScreen = (): JSX.Element => {
             fullWidth
           />
         </div>
-        <div style={{margin: `10px 0`}}>
+        <div
+          style={{display: 'flex', flexDirection: 'row', alignItems: 'center', margin: `10px 0`}}>
           <Button
             variant="contained"
             color="primary"
@@ -56,6 +68,29 @@ export const SettingsScreen = (): JSX.Element => {
             onClick={() => setFormValues(settings)}>
             Reset
           </Button>
+          <Button
+            style={{marginLeft: 10}}
+            variant="contained"
+            color="default"
+            disabled={testState === TestState.Started}
+            onClick={async () => {
+              setTestState(TestState.Started)
+              const response = await fetch('/api/remote/calls/123456789/status?timeout=5000', {
+                headers: {
+                  Authorization: `bearer ${formValues.accessToken}`,
+                  'x-remote-proxy-destination': formValues.remoteApiOrigin,
+                },
+              })
+
+              setTestState(response.ok ? TestState.Passed : TestState.Failed)
+            }}>
+            Test
+          </Button>
+          <div style={{marginLeft: 10}}>
+            {testState === TestState.Started ? <HourglassIcon /> : null}
+            {testState === TestState.Passed ? <CheckIcon /> : null}
+            {testState === TestState.Failed ? <ErrorIcon /> : null}
+          </div>
         </div>
       </form>
     </div>
